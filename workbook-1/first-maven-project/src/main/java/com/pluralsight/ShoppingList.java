ecee97dd54267ff25c3035cebc3687adbdebe305
Siry Siu <sewsiri@gmail.com>
2024-09-26 09:05:53 -0400

    completed ShoppingList App

---
+++ workbook-1/first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] arg) {
+    String[] shoppingList = {"Eggs", "Milk", "Butter", "Soup", "Soap",
+            "Candy", "Water", "Juice", "Shampoo"};
+    for (int i = 0; i < shoppingList.length; i++) {
+        System.out.println(shoppingList[i]);
+        }
+    }
+}
